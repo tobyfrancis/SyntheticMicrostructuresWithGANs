fixed rotation again again

code/loading.py
@@ -4,10 +4,10 @@ import crystallography as xtal
 import pandas as pd
 import gc
 
-def rotate(dataset,big_center,small_center,wlh,rot):
+def rotate(dataset,big_center,small_center,wlh,rotation_matrix):
     def rot(index):
         ijk = small_center - np.matrix(np.unravel_index(index,wlh)).reshape(3,1)   
-        hkl = np.array(np.round(big_center - rot*ijk),dtype=int)
+        hkl = np.array(np.round(big_center - rotation_matrix*ijk),dtype=int)
         return dataset[hkl]
 
     return rot
@@ -61,7 +61,7 @@ def load_quats(filename,sym):
     return dataset.reshape(shape)
 
 def random_rotated_cube(dataset):
-    rot = xtal.cu2om(xtal.randomOrientations(1))
+    rotation_matrix = xtal.cu2om(xtal.randomOrientations(1))
 
     d = min(dataset.shape[0],dataset.shape[1],dataset.shape[2]) #diameter of sphere in prism
     a = 2*np.sqrt(d**2/12) #inner cube dimension
@@ -71,7 +71,7 @@ def random_rotated_cube(dataset):
 
     small_center = np.matrix(wlh/2).reshape(3,1) #center of inner cube
     big_center = np.matrix([dataset.shape[0]/2,dataset.shape[1]/2,dataset.shape[2]/2]).reshape(1,3)
-    rot_func = rotate(dataset,big_center,small_center,wlh,rot)
+    rot_func = rotate(dataset,big_center,small_center,wlh,rotation_matrix)
     return np.array(list(map(rot_func,index_array))).reshape(wlh[0],wlh[1],wlh[2],4)
 
 # TODO: